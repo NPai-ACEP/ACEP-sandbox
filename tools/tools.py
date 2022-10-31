aab4f6153a27ed9190c408347e80f5ba8a885e01
npai-acep <npai@acep.org>
2022-10-31 13:31:44 -0500

    Minor changes for clarity

--- tools/tools.py
+++ tools/tools.py
@@ -1,5 +1,7 @@
-import json,requests,os,sys
+import os.path
+
 import config
+import requests
 
 api_url = "https://api.monday.com/v2"
 headers = {"Authorization": config.api_key_monday}
@@ -7,19 +9,20 @@ headers = {"Authorization": config.api_key_monday}
 
 def write_data(**all_data):
     for data_name, data_item in all_data.items():
-        file_name = os.path.join(os.getcwd(),'data')
-        os.makedirs(file_name,exist_ok=True)
+        file_name = os.path.join(os.getcwd(), 'data')
+        os.makedirs(file_name, exist_ok=True)
         with open(f'{file_name}/{data_name}.txt', 'w', encoding='utf8') as f:
             print(data_item, file=f)
-            
-            
+
+
 def query_helper(query, variables, silent=1):
     data = {'query': query, 'variables': variables}
     r = requests.post(url=api_url, json=data, headers=headers)
-    if silent==False:
+    if silent is False:
         print(data)
         print(r.json())
     return r.json()
+
 
 def titlecase_formater(item_list):
     new_item_list = []
